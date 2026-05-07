6ee58ff767beab88e0905334b651b331ce588f9a
NiklasLE34 <59610388+NiklasLE34@users.noreply.github.com>
2026-05-07 20:02:09 +0200

    grid.css - middle ul defines left rule

--- js/left-rule.js
+++ js/left-rule.js
@@ -1,1 +1,31 @@
+(function () {
+  function updateGridLeftRule() {
+    const ul = document.querySelector('#header .main-header__nav > ul');
+    if (!ul) return;
 
+    const left = Math.round(ul.getBoundingClientRect().left);
+    const asideWidth = left - 40;
+
+    document.documentElement.style.setProperty(
+      '--grid-aside-width',
+      asideWidth + 'px'
+    );
+  }
+
+  let raf;
+
+  function scheduleUpdate() {
+    cancelAnimationFrame(raf);
+    raf = requestAnimationFrame(updateGridLeftRule);
+  }
+
+  window.addEventListener('resize', scheduleUpdate);
+  window.addEventListener('load', scheduleUpdate);
+  document.addEventListener('DOMContentLoaded', scheduleUpdate);
+
+  setTimeout(scheduleUpdate, 100);
+  setTimeout(scheduleUpdate, 500);
+  setTimeout(scheduleUpdate, 1000);
+
+  scheduleUpdate();
+})();
